0ab24feed1f8ca6ee03d271b84859f7b8c08aa43
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-01-28 09:37:43 -0300

    ignore

--- conection.php
+++ conection.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', '127.0.0.1');
-define('USER', 'root');
-define('PASSWORD', 'root');
+define('USER', 'login');
+define('PASSWORD', 'login@748596');
 define('DB', 'phpcrud');
 
 $conection = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('Can not connect.');
