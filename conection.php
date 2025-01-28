b56f5648e50af51ae59592fc0734ab8619a5836e
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-01-28 10:22:24 -0300

    connection

--- conection.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', '127.0.0.1:3306');
-define('USER', 'root');
-define('PASSWORD', 'root');
-define('DB', 'phpcrud');
-
-$connection = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('Can not connect.');
-?>
