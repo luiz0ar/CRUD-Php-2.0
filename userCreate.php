a618d1e2db51dc158208ebce44e6a44690387361
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-01-28 11:35:57 -0300

    create

--- userCreate.php
+++ userCreate.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Usuário - Criar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
-    <?php include('navbar.php'); ?>
+  <?php include('navbar.php'); ?>
     <div class="container mt-4">
-    <?php include('message.php'); ?>
-      <div class="row">
+      <?php include('message.php'); ?>
+      <div class="row">     
         <div class="col-md-12">
           <div class="card">
             <div class="card-header">
